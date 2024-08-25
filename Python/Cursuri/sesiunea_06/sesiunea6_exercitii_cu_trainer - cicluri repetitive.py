122828ef3a7b426d7b23fe0f86bdd40e38bcacdf
ConstantinIosef <cconstantin.c.iosef@gmail.com>
2024-08-25 12:41:50 +0300

    push1

---
+++ Python/Cursuri/sesiunea_06/sesiunea6_exercitii_cu_trainer - cicluri repetitive.py
@@ -0,0 +1,83 @@
+"""
+Sesiunea 6 - Exercitii cu Trainer
+"""
+
+
+"""
+1. GHICITOARE DE NUMAR
+- numar_secret = Generati un numar random intre 1 si 30
+- numar_ghicit = None
+- Folosind un while:
+   - Utilizatorul alege un numar
+   - Programul îi spune (in functie de caz):
+        - Nr secret e mai mare
+        - Nr secret e mai mic
+        - Felicitari! Ai ghicit!
+"""
+
+
+"""
+2. Alege un numar de la tastatura si genereaza in consola o piramida
+de numere, ca in exemplele de mai jos: 
+
+Exemplu: 7
+
+1
+22
+333
+4444
+55555
+666666
+7777777
+
+Exemplu: 3
+
+1
+22
+333
+"""
+
+
+"""
+3. Se da lista:
+tastatura_telefon = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [0]
+]
+
+Itereaza prin lista 2D si printeaza ‘Cifra curenta este x.’
+(HINT: nested for - adică for in for)
+"""
+
+
+"""
+4. Se da lista:
+numere = [23, -45, 79, -236, 200, -13]
+
+Creeaza o lista care contine doar numerele negative.
+"""
+
+
+"""
+5. 
+Se da dictionarul:
+persoana = {
+    'nume': 'Alex',
+    'varsta': 25,
+    'oras': 'Bucuresti'
+}
+
+a. Cere utilizatorului cheia si valoarea pe care doreste sa o
+adauge in dictionar. Foloseste metoda update().
+b. Printeaza, pe rand, toate cheile din dictionar.
+c. Printeaza, pe rand, toate valorile din dictionar.
+"""
+
+
+"""
+6. Cere utilizatorului sa introduca de la tastatura un numar intreg pozitiv.
+Scrie un program care verifica daca numarul este prim.
+Afiseaza un mesaj sugestiv in consola in fiecare caz.
+"""
---
+++ Python/Cursuri/sesiunea_06/sesiunea6_exercitii_cu_trainer - structuri de date.py
@@ -0,0 +1,80 @@
+"""
+Sesiunea 6 - Exercitii cu Trainer
+"""
+
+
+"""
+1. Lista de cumparaturi:
+
+Se citeste de la tastatura o lista de cumparaturi. 
+Utilizatorul introduce lista de cumparaturi ca un string,
+cu alimentele separate prin virgula,
+ATENTIE: fara spatii
+Exemplu:rosii,castraveti,branza,oua
+
+a. Sa se transforme string-ul citit de la tastatura intr-o lista. (vezi metode ajutatoare string).
+b. Sorteaza lista de cumparaturi si printeaza lista sortata.
+c. Adauga un aliment nou in lista de cumparaturi.
+d. Sterge un aliment din lista de cumparaturi.
+e. Modifica elementul de la pozitia 0 din lista.
+f. Daca lista contine 'rosii' in ea, afiseaza mesajul "Aliment sanatos".
+Daca nu, afiseaza mesajul "Iti recomandam rosiile de asemenea".
+"""
+
+
+"""
+2. Se da lista fructe = ['capsuni', 'mere', 'lamai'].
+Converteste lista la string si afiseaza string-ul.
+A se vedea metoda join().
+"""
+
+
+"""
+3. Se da lista numere = [1, 2, 3, 4, 56, 22, 5].
+Afiseaza elementul cu valoarea maxima din string. (google- functia max())
+"""
+
+
+"""
+4. Se da lista preturi = [12.3, 34.5, 22].
+Calculeaza suma elementelor din lista preturi. (google - functia sum())
+"""
+
+
+"""
+5. Se da dictionarul:
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+a. Afiseaza valoarea cheii city.
+b. Kelly a fost promovata. Salariul ei este acum de 10000 lei. Fa modificarea si in dictionar.
+c. Sterge varsta din dictionar.
+d. Adauga o noua pereche cheie-valoare in dictionar, cu cheia employment_date. Valoarea o alegi tu.
+e. Verifica daca exista cheia 'country' in dictionar. Daca nu exista, adauga-o.
+"""
+
+
+"""
+6. Concatenarea a doua dictionare
+
+Se dau doua dictioanare:
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'c': 3, 'd': 4}
+
+Sa se concateneze cele doua dictionare folosind metoda update().
+Observatie: Metoda update() o putem folosi pentru a adauga unul sau mai multe
+perechi cheie:valoare in dictionar.
+"""
+
+
+"""
+7. Se da urmatoarea lista cu dictionare:
+lista = [{'a': 1, 'b': 2}, {'c': 3, 'd': 4}, {'e': 5, 'f': 6}]
+a. Adauga perechea {'c':'3'} in primul dictionar din lista.
+b. Adauga un nou dictionar ca element in lista. Adauga-l la final.
+c. Aduaga un nou dictionar ca element in lista, la indexul 1.
+d. Verifica daca in al doilea dictionar din lista se gaseste cheia 'c'.
+"""

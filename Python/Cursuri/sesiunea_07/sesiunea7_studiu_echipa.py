4e1a82010425712ef3ec77ddd50e2dd537657c58
ConstantinIosef <cconstantin.c.iosef@gmail.com>
2024-08-25 13:20:50 +0300

    push2

---
+++ Python/Cursuri/sesiunea_07/sesiunea7_studiu_echipa.py
@@ -0,0 +1,51 @@
+"""
+EXERCITII WORKSHOP (Sesiunea 7)
+"""
+
+
+"""
+! Pentru toate exercițiile: Apelati functia de cel putin 2 ori cu valori diferite pentru a testa.
+Daca functia are return, printeaza raspunsul.
+"""
+
+
+"""
+1. Scrieti o functie care primeste ca parametru lungimea laturii
+unui patrat si returneaza aria sa.
+"""
+
+
+"""
+2. Scrieti un subprogram care primeste ca parametru lungimea laturii unui patrat
+si returneaza atat lungimea diagonalei, cat si perimetrul patratului.
+"""
+
+
+"""
+3. Scrieti o functie care primeste ca parametri de intrare lungimile celor doua catete
+ale unui triunghi dreptunghic si returneaza lungimea ipotenuzei.
+"""
+
+
+"""
+4. Scrieti o functie care primeste 3 parametri de tip real, cu semnificatia
+de lungimi pentru 3 segmente. 
+Functia va returna 1 daca cele trei segmente pot forma un triunghi si 0, in caz contrar.
+"""
+
+
+"""
+5. Functie care returneaza aria cercului.
+"""
+
+
+"""
+6. Functie fara return, primeste un string si printeaza pe ecran:
+Numarul de caractere lower case este x
+Numarul de caractere upper case este y 
+"""
+
+
+"""
+7. Functie care primeste o LISTA de numere si returneaza o LISTA doar cu numerele pozitive.
+"""
---
+++ Python/Cursuri/sesiunea_08/sesiunea8_studiu_echipa.py
@@ -0,0 +1,30 @@
+"""
+EXERCITII WORKSHOP (Sesiunea 8)
+"""
+
+
+"""
+1.Implementeaza clasa Cerc:
+- atribute: rază, culoare
+- constructor pentru ambele atribute
+- metode:
+    - descrie_cerc() - va PRINTA culoarea și raza
+    - aria() - va RETURNA aria 
+    - diametru() 
+    - circumferinta()
+"""
+
+
+"""
+2. Implementeaza clasa Dreptunghi:
+- atribute: lungime, lățime, culoare
+- constructor pentru toate atributele
+- metode:
+    - descrie()
+    - aria()
+    - perimetrul()
+    - schimbă_culoarea(noua_culoare):
+        - această metodă nu returneaza nimic. 
+        - Ea va lua ca parametru o noua culoare si va suprascrie atributul self.culoare.
+        - Poti verifica schimbarea culorii prin apelarea metodei descrie().
+"""

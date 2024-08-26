12e3dc11d1aaf89238e22a41b66729072900763e
ConstantinIosef <constantin.c.iosef@gmail.com>
2024-08-26 17:17:11 +0100

    push5

---
+++ Python/Cursuri/sesiunea_01/clasa/rezolvari_exercitii01.py
@@ -0,0 +1,75 @@
+"""
+EX1:
+a. Defineste o variabila de tip int, numita 'latime'.
+b. Defineste o variabila de tip string, numita 'descriere'.
+c. Defineste 2 variabile de tip float, numite 'pret' si 'discount'.
+d. Defineste o variabila de tip bool, numita 'initializat' care are valoarea True.
+e. Printeaza variabilele definite la punctele anterioare.
+"""
+
+
+"""
+EX2: Folosind o singura linie de cod, defineste 2 variabile, de tip int, cu valoarea 10.
+"""
+
+
+"""
+EX3: Folosind o singura linie de cod, initializeaza/ defineste doua variabile de tip string cu valori diferite.
+"""
+
+
+"""
+EX4: Defineste doua variabile de tip string, numite 'nume', respectiv 'pret'.
+Afiseaza in consola un mesaj care sa contina cele doua variabile.
+"""
+
+
+"""
+EX5:
+a. Defineste doua variabile: nume (string) si varsta (int).
+b. Folosind f-string, afiseaza in consola, o propozitie care sa contina cele doua variabile.
+"""
+
+
+"""
+EX6:
+a. Defineste o variabila de tip int, afiseaz-o in consola. Afiseaza de asemenea si tipul acesteia.
+b. Defineste o variabila de tip float, afiseaz-o in consola. Afiseaza de asemenea si tipul acesteia.
+c. Defineste o variabila de tip string, afiseaz-o in consola. Afiseaza de asemenea si tipul acesteia.
+d. Defineste o variabila de tip bool, afiseaz-o in consola. Afiseaza de asemenea si tipul acesteia.
+"""
+
+
+"""
+EX7: 
+a. Defineste o variabila de tip int. Afiseaz-o in consola.
+b. Afiseaza in consola tipul variabilei definite la punctul a, folosind functia type().
+c. Converteste variabila de tip int de la punctul a, la tipul float si salveaza rezultatul intr-o alta variabila.
+d. Afiseaza in consola tipul variabilei generate la punctul c.
+"""
+
+
+"""
+EX8:
+a. Defineste o variabila de tip string. Afiseaz-o in consola.
+b. Afiseaza in consola tipul variabilei definite la punctul a, folosind functia type().
+c. Converteste variabila de la punctul a, in int si salveaza rezultatul intr-o noua variabila.
+Ruleaza programul.
+Ce observi?
+"""
+
+
+"""
+EX9: Citeste de la tastatura un nume de produs.
+Salveaza rezultatul intr-o variabila.
+Afiseaza un mesaj care sa contina variabila salvata.
+"""
+
+
+"""
+EX10: Citeste de la tastatura un pret. Obliga utilizatorul sa introduca pretul ca si numar zecimal.
+Salveaza rezultatul intr-o variabila.
+Afiseaza un mesaj care sa contina variabila salvata.
+"""
+
+

33c41d460b08e45f69acba18933c84b1e20b39e0
ConstantinIosef <constantin.c.iosef@gmail.com>
2024-08-27 18:53:03 +0100

    creat fisiere de exercitii

---
+++ Python/Cursuri/sesiunea_07/clasa/exercitii07.py
@@ -0,0 +1,63 @@
+"""
+EX1: Defineste o functie care printeaza, pe rand,
+primele 10 numere (1, 10).
+"""
+
+
+
+"""
+EX2: Scrie o functie care parcurge o lista de numere data si
+afiseaza mesajul 'Este par' pentru numerele pare si
+'Este impar' pentru numere impare.
+Daca in lista se gaseste un element care nu e numar intreg,
+afiseaza un mesaj utilizatorului si apoi sari peste
+elementul respectiv.
+(Foloseste functia built-in isinstance()
+pentru verificare daca elementul curent e int sau nu)
+"""
+
+
+
+"""
+EX3: Scrie o functie care calculeaza patratul unui numar.
+Afiseaza rezultatul.
+"""
+
+
+
+"""
+EX4: Scrie o functie care calculeaza impartirea dintre doua numere.
+Afiseaza rezultatul.
+"""
+
+
+
+"""
+EX5: Scrie o functie care calculeaza inmultirea dintre doua numere.
+Afiseaza rezultatul.
+"""
+
+
+
+"""
+EX6: Rescrie functia de la exercitiul 3, 
+astfel incat sa returneze rezultatul.
+"""
+
+
+
+"""
+EX7: Rescrie functia de la exercitiul 4, 
+astfel incat sa returneze rezultatul.
+"""
+
+
+
+"""
+EX8: Scrie o functie care ia ca parametru un numar intreg
+si returneaza True daca numarul e par
+si False daca numarul e impar.
+"""
+
+
+

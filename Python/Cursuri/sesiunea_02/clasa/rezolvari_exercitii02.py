59e15fa8f6778d91f26f757d97c5afa9ce986a9e
ConstantinIosef <constantin.c.iosef@gmail.com>
2024-08-27 17:16:18 +0100

    sesiunea 2 exercitii

---
+++ Python/Cursuri/sesiunea_02/clasa/rezolvari_exercitii02.py
@@ -0,0 +1,155 @@
+"""
+EX1: Se da string-ul prop3 = 'Concertul va avea loc maine."
+a. Salveaza intr-o variabila, folosind slicing, primul cuvant.
+b. Extrage primele 3 caractere din prop3.
+c. Afiseaza prop3 cu caracterele inversate.
+"""
+
+
+
+"""
+EX2: Se da variabila prop1 = 'Andy este prescurtarea de la Andrei"
+a. Afiseaza primul caracter.
+b. Afiseaza al 4-lea caracter.
+c. Afiseaza ultimul caracter.
+"""
+
+
+
+"""
+EX3: Se da string-ul prop2 = 'Masina e rosie.'
+Afiseaza lungimea string-ului prop2.
+"""
+
+
+
+"""
+EX4: Se da string-ul my_str = 'vacanta'.
+a. Foloseste metoda find() pentru a afla primul index la care se gaseste caracterul 'a'.
+b. Foloseste metoda count() pentru a afla de cate ori apare caracterul 'a' in my_str.
+c. Foloseste metoda capitalize() pentru a scrie cuvantul cu prima litera mare.
+d. Foloseste metoda upper() pentru a scrie cuvantul cu litere mari.
+"""
+
+
+
+"""
+EX5: Exploreaza urmatoarele metode ajutatoare ale string-ului:
+a. endswith()
+b. index()
+c. lower()
+d. replace()
+e. strip()
+"""
+
+
+
+"""
+EX6: Se dau doua variabile, a = 10, b = 2.
+Efectueaza toate operatiile pe cele 2 variabile,
+folosind operatorii aritmetici.
+"""
+
+
+
+"""
+EX7: Pentru fiecare din exemplele de mai jos, scrie intr-un comentariu
+rezultatul asteptat, apoi decomenteaza codul de la fiecare exemplu, pe rand
+si ruleaza exemplele si verifica output-ul.
+"""
+
+# Exemplul 1:
+a = True
+b = False
+# print(not(a))
+# print(not(b))
+
+# Exemplul 2:
+a = True
+b = False
+# x = not(a)
+# y = not(b)
+# print(a or b)
+# print(x or y)
+# print(a or x)
+# print(x or b)
+
+# Exemplul 3:
+a = False
+b = False
+# x = not(a)
+# y = not(b)
+# print(a and b)
+# print(a and x)
+# print(y and b)
+# print(x and y)
+
+
+
+"""
+EX8: Se da variabila num = 12
+a. Verifica daca num citit este pozitiv.
+b. verifica daca num este mai mare decat 5.
+verifica daca num este mai mic decat 25.
+c. verifica daca num este intre 0 si 20.
+"""
+
+
+
+"""
+EX9: Verifica daca varsta introdusa de utilizator este mai mare
+decat 18 ani.
+"""
+
+
+
+"""
+EX10: Verifica daca pretul introdus de utilizator este mai mic sau egal cu 100 de dolari.
+"""
+
+
+
+"""
+EX11:
+a. Citeste un numar de la tastatura.
+b. Verifica daca numarul este par sau impar si afiseaza un mesaj sugestiv
+# in fiecare situatie.
+"""
+
+
+
+"""
+EX12:
+a. Citeste de la tastatura viteza medie cu care conduce utilizatorul.
+b. Daca viteza este sub 50 sau egala cu 50, afiseaza mesajul "Viteza normala".
+c. Daca viteza este mai mare de 50, afiseaza mesajul "Viteza depasita".
+"""
+
+
+
+"""
+EX13: Citeste de la tastatura varsta utilizatorului si afiseaza categoria
+de varsta in care se incadreaza.
+Tine cont de aceste categorii de varsta:
+- intre 0-18 ani: minor
+- intre 18-65 ani: adult
+- peste 65 ani: senior
+"""
+
+
+
+"""
+EX14: Saptamana aceasta, supermarket-ul X ofera clientilor o reducere la intreg
+cosul de cumparaturi, in functie de totalul cosului de cumparaturi
+Reducerea se aplica in felul urmator:
+- Total este intre 100 si 200 lei -> reducere 10%
+- Total intre 200 - 300 lei -> reducere 15%
+- Total intre 300-400 -> reducere 20 %
+- Total mai mare de 400 -> reducere 30 %.
+a. Citeste de la tastatura totalul cosului de cumparaturi al utilizatorului.
+b. Afiseaza pretul pe care utilizatorul trebuie sa il plateasca pe cumparaturi
+dupa aplicarea reducerii.
+"""
+
+
+
